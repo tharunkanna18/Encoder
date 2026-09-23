0ccf2bb45cc9a154882565e5eb6e6ef67de78e94
Tharun kanna <tarunbabu376@gmail.com>
2026-09-23 18:57:46 +0530

    Add Core/Inc/telnet_server.h

---
+++ Core/Inc/telnet_server.h
@@ -0,0 +1,19 @@
+/*
+ * telnet_server.h
+ *
+ *  Created on: Jun 3, 2026
+ *      Author: user
+ */
+
+#ifndef INC_TELNET_SERVER_H_
+#define INC_TELNET_SERVER_H_
+
+
+// ========================================
+// Function Prototypes
+// ========================================
+void Telnet_Server_Init(void);
+
+void Telnet_Send(char *msg);
+
+#endif /* INC_TELNET_SERVER_H_ */
